2102cea07d631cadb9d6590c31b90efd0f16ee63
masahirokato <hcooh18@gmail.com>
2020-03-16 19:03:38 +0900

    add link_to

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@
 Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
+<<<<<<< HEAD
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
   resources :books
+=======
+  get  '/about',  to: 'static_pages#about'
+>>>>>>> add link_to
 end
