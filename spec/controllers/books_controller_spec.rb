d16a653e962333fb0aac95b1df08b1514be5831d
TaigaMikami <mikamimikami60@gmail.com>
2020-03-17 19:07:24 +0900

    remove rspec test

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,17 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-  describe 'GET #new' do
-    it 'returns http success' do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
