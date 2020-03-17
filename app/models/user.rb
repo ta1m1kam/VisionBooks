779b56f7c3fd82a56afc834d56a1811136227dc1
masahirokato <hcooh18@gmail.com>
2020-03-17 17:28:17 +0900

    add plus

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@chiba-u.jp/
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
