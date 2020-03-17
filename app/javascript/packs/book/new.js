c4fe2fc7495544bf2820b033c63fe8357a79c700
TaigaMikami <mikamimikami60@gmail.com>
2020-03-17 20:01:57 +0900

    add quagga.js

--- app/javascript/packs/book/new.js
+++ app/javascript/packs/book/new.js
@@ -84,4 +84,47 @@ document.addEventListener('DOMContentLoaded', () => {
       return true
     }
   }
+
+  Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["ean_reader"]
+    }
+  }, function(err) {
+    if (err) {
+      console.log(err);
+      return
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+  });
+
+  Quagga.onDetected(success => {
+    const code = success.codeResult.code;
+    if(calc(code)) alert(code);
+  })
+
+  const calc = isbn => {
+    const arrIsbn = isbn
+      .toString()
+      .split("")
+      .map(num => parseInt(num));
+    let remainder = 0;
+    const checkDigit = arrIsbn.pop();
+
+    arrIsbn.forEach((num, index) => {
+      remainder += num * (index % 2 === 0 ? 1 : 3);
+    });
+    remainder %= 10;
+    remainder = remainder === 0 ? 0 : 10 - remainder;
+
+    return checkDigit === remainder;
+  }
+
 });
+
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,9 +2,10 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
+    Quagga: 'quagga/dist/quagga.js',
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default'],
   })
 )
 
