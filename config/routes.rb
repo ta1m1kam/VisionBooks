7a68dd6264f1f4fe79c5f70f83616d3ffc59f758
TaigaMikami <mikamimikami60@gmail.com>
2020-03-17 15:15:31 +0900

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
   get  '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
   resources :users
 end
