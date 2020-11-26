d4bf0cc8153e9b3e17c93b1980b9c494daf6d727
masahirokato <hcooh18@gmail.com>
2020-11-26 19:05:31 +0900

    add cloudinary

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,9 +2,10 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+
+  include Cloudinary::CarrierWave if Rails.env.production?
 
   # Choose what kind of storage to use for this uploader:
   include CarrierWave::MiniMagick
